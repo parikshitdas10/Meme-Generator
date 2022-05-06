0a1072ddbb5703141d0737f0c9ed57e8fb606d17
parikshitdas10 <parikshitdas10@gmail.com>
2022-05-06 15:05:32 +0530

    updated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,23 +2,66 @@ import React from 'react'
 import memesData from '../memesData'
 
 function Meme() {
-  let [url,setURL]=React.useState("")
+  const [formData,setFormData]=React.useState({
+    upperText:"",
+    lowerText:""
+  })
+  const [allMemeImages,setAllImages]=React.useState(memesData)
+  let [meme,setMeme]=React.useState({
+    topText:"",
+    bottomText:"",
+    randomImage:"https://i.imgflip.com/2d3al6.jpg"
+  })
   function get_random (list) {
     return list[Math.floor((Math.random()*list.length))];
   }
-  function set(){
-    const arr=memesData.data.memes
+  function setRandom(){
+    const arr=allMemeImages.data.memes
     const item = get_random(arr)
-    setURL(`${item.url}`)
+    const url=item.url
+    setMeme(prev=>{
+      return ({
+        ...prev,
+        randomImage:url
+      })
+    })
+  
   }
+  function handleChange(event){
+  const {name,value}=event.target
+  setFormData(prev=>{
+    return{
+      ...prev,
+      [name]:value
+    }
+  })
+  }
+  
   return (
     <div className='Meme'>
         <div className='form'>
-            <input type="text" className='upper-text' placeholder='Upper Text'/>
-            <input type="text" className='lower-text' placeholder='Lower Text'/>
+            <input 
+            type="text" 
+            className='upper-text' 
+            placeholder='Upper Text'
+            value={formData.upperText}
+            onChange={handleChange}
+            name="upperText"
+            />
+            <input type="text"
+              className='lower-text'
+              placeholder='Lower Text'
+              value={formData.lowerText}
+              onChange={handleChange}
+              name="lowerText"
+              />
         </div>
-        <button onClick={set} className='btn'>Get a new meme image</button>
-        <img className='meme-img' src={url}/>
+        <button onClick={setRandom} className='btn'>Get a new meme image</button>
+        <div className='img-container'>
+        <img className='meme-img' src={meme.randomImage}/>
+        <h1 className='firstText'>{formData.upperText}</h1>
+        <h1 className='secondText'>{formData.lowerText}</h1>
+        </div>
     </div>
   )
 }
