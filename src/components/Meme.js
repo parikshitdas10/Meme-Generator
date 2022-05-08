8353db6c2dc53e4387df9300779066f0978ae2f8
parikshitdas10 <parikshitdas10@gmail.com>
2022-05-08 12:48:06 +0530

    now fetching data directly from imgflip API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import memesData from '../memesData'
+
 
 function Meme() {
   const [formData,setFormData]=React.useState({
     upperText:"",
     lowerText:""
   })
-  const [allMemeImages,setAllImages]=React.useState(memesData)
-  let [meme,setMeme]=React.useState({
+
+
+  const [allMemeImages,setAllImages]=React.useState([])
+
+
+  React.useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res=>res.json())
+    .then(data=>setAllImages(data.data.memes))
+  },[])
+  
+  const [meme,setMeme]=React.useState({
     topText:"",
     bottomText:"",
     randomImage:"https://i.imgflip.com/2d3al6.jpg"
@@ -16,7 +26,7 @@ function Meme() {
     return list[Math.floor((Math.random()*list.length))];
   }
   function setRandom(){
-    const arr=allMemeImages.data.memes
+    const arr=allMemeImages
     const item = get_random(arr)
     const url=item.url
     setMeme(prev=>{
@@ -58,7 +68,7 @@ function Meme() {
         </div>
         <button onClick={setRandom} className='btn'>Get a new meme image</button>
         <div className='img-container'>
-        <img className='meme-img' src={meme.randomImage}/>
+        <img className='meme-img' src={meme.randomImage} alt="meme img"/>
         <h1 className='firstText'>{formData.upperText}</h1>
         <h1 className='secondText'>{formData.lowerText}</h1>
         </div>
